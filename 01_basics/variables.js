b080f181ab8740023e1b3dbf7e82232fe8c68236
Fahad Khan <156856776+Fahadkhan0047@users.noreply.github.com>
2024-01-22 14:20:33 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 223456
+let accountName = "Fahad"
+let accountPassword = "123456"
+var accountCity = "Banglore"
+accountCity = "Jaipur"
+
+console.log(accountCity);
+
+console.table([accountId,accountName,accountPassword,accountCity])
