87ab49811c754d61eb152ebe0b51939f47899d2c
Fahad Khan <156856776+Fahadkhan0047@users.noreply.github.com>
2024-01-22 14:59:39 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict" // treat all the javacsript code as newer version
+
+// alert(3+3) used in browser but not in node
+
+// console.log(3+3); this is the way to write the above code in node environment
+
+let name = "Fahad"
+let age = 21
+let isLoggedIn = false
+let state = null
+let phone
+
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(isLoggedIn));
+console.log(typeof(state));
+console.log(typeof(phone));
+
+
+// DATA TYPES
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value / Object
+// undefined => undefined
