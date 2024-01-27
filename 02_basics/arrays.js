93994a14a0c799fe3d0e40ad5d8b35317ebeb1a1
Fahad Khan <156856776+Fahadkhan0047@users.noreply.github.com>
2024-01-27 08:05:26 +0000

    arrayspt1

---
+++ 02_basics/arrays.js
@@ -0,0 +1,45 @@
+                          //Array Basics
+
+
+const myArr = [1,2,3,4,5]
+const myHeros = ["Batman","Captain America","Iron Man"]
+
+// console.log(myArr[2]);
+
+// console.log(myHeros[0]);
+
+
+
+                          // Arrays Methods
+
+
+
+// myArr.push(6)
+// myArr.push(10)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(2));
+// console.log(myArr.indexOf(4));
+
+// const newArr = myArr.join()
+// console.log(newArr);
+// console.log(typeof(newArr));
+
+
+                 //(slice)-> takes n-1 range and does not manipulate the orginal array 
+
+
+// const myn1 = myArr.slice(1,3)  
+// console.log("A-->",myn1);
+// console.log(myArr);
+                
+
+                //(splice)-> take full range upto n no of elements and manipulates the array
+
+
+// const myn2 = myArr.splice(1,3)
+// console.log("B-->",myn2);
+// console.log(myArr);
