3c6741cabfe73c744a9f7e8c0e3421a309134bba
Fahad Khan <156856776+Fahadkhan0047@users.noreply.github.com>
2024-03-07 19:42:14 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,16 @@
+let i= 0;
+while (i<=10) {
+
+    // console.log(`Value of ${i}`);
+    i++;
+    
+}
+
+let myArray = ['flash','superman','batman']
+
+let arr= 0;
+while (arr<myArray.length) {
+    console.log(`the value of arr is ${myArray[arr]}`);
+    arr= arr+1;
+    
+}
