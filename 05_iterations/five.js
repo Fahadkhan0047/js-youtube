fd977124d352b00d6e655911180b82085287a131
Fahad Khan <156856776+Fahadkhan0047@users.noreply.github.com>
2024-03-07 20:39:29 +0000

    for-each-loop

---
+++ 05_iterations/five.js
@@ -0,0 +1,52 @@
+
+          // For Each loop
+
+          
+
+const coding = ['java','ruby','python','javascript']
+
+// coding.forEach(function (val){
+//     console.log(val);
+
+// })
+
+
+// coding.forEach((val) => {
+//     console.log(val);
+// })
+
+// function printMe(item)
+// {
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach((item,index,arr) => {
+//     console.log(item,index,arr);
+
+// })
+
+
+    //   [{},{},{}]
+
+
+const myCoding = [ 
+{
+    languageName:'Javascript',
+    FileName:'js'
+},
+{
+    languageName:'Java',
+    FileName:'java'
+},
+{
+    languageName:'Python',
+    FileName:'py'
+},
+
+]
+
+myCoding.forEach((item) => {
+    console.log(item.languageName);
+})
